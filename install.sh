13347b77272a09aa6be3f9181b0c38b814918786
Dom Amato <dom@hologram.io>
2019-07-05 13:56:40 -0500

    update install/service files to reflect new layout

--- install.sh
+++ install.sh
@@ -17,13 +17,13 @@ sudo apt-get update && \
 	pkg-config \
     git
 
-git clone https://github.com/DomAmato/magic-agent
+git clone https://github.com/magic-network/magic-agent
 cd magic-agent
 
 export MAGIC_LOC=$PWD
 
+# Required for freeradius to work
 pip install future
-pip3 install pylint
 
 mv ./conf/user-config.hjson ./magic/gateway/config
 
@@ -32,14 +32,14 @@ mv ./conf/user-config.hjson ./magic/gateway/config
 pip3 install .
 
 # Move the resources into place
-mv ./magic/resources/inner-tunnel /etc/freeradius/3.0/sites-enabled/inner-tunnel
-mv ./magic/resources/python-magic /etc/freeradius/3.0/mods-enabled/python-magic
+mv ./resources/inner-tunnel /etc/freeradius/3.0/sites-enabled/inner-tunnel
+mv ./resources/python-magic /etc/freeradius/3.0/mods-enabled/python-magic
 sed -i "s@MAGIC_LOC@"${MAGIC_LOC}"@g" /etc/freeradius/3.0/mods-enabled/python-magic
-mv ./magic/resources/eap /etc/freeradius/3.0/mods-enabled/eap
-mv ./magic/resources/clients.conf /etc/freeradius/3.0/clients.conf
-
+mv ./resources/eap /etc/freeradius/3.0/mods-enabled/eap
+mv ./resources/clients.conf /etc/freeradius/3.0/clients.conf
 mv ssl/* /etc/freeradius/3.0/certs/
 
+# Add services to startup 
 sed -i "s@MAGIC_LOC@"${MAGIC_LOC}"@g" ./magic.service
 sudo mv ./magic.service /lib/systemd/system/magic.service
 sudo mv ./freeradius.service /lib/systemd/system/freeradius.service
@@ -48,4 +48,5 @@ sudo systemctl daemon-reload
 sudo systemctl enable magic.service
 sudo systemctl enable freeradius.service
 
+#reboot to enable changes 
 sudo reboot
